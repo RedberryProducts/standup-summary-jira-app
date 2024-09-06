b1a11dd0e671b74e06d9b533614b92333284e0e6
giunashvili <giorgi.official@yahoo.com>
2024-09-06 16:38:51 +0400

    finalize workflow

--- .github/helpers/remove-display-conditions.py
+++ .github/helpers/remove-display-conditions.py
@@ -2,7 +2,7 @@ import yaml
 
 with open('manifest.yml') as f:
     doc = yaml.safe_load(f)
-doc['modules']['jira:projectPage'][0]['displayConditions'] = None
+del doc['modules']['jira:projectPage'][0]['displayConditions']
 
 with open('manifest.yml', 'w+') as f:
     yaml.dump(doc, f)
