b7fd38e0ee87f16a6fd8a8980043500f87201599
ltchelishvili1 <tchelishvililevani@gmail.com>
2024-09-15 21:38:48 +0400

    REPAUTO-41: set up default statuses & keep them on refresh

--- src/frontend/components/Settings/useSettings.js
+++ src/frontend/components/Settings/useSettings.js
@@ -1,19 +1,42 @@
 import { invoke } from "@forge/bridge";
 import { useEffect, useState } from "react";
 import useIndex from "../../useIndex";
+import { DEFAULT_STATUSSES } from "../../utils";
 
 const useSettings = () => {
   const [slackEndpoint, setSlackEndpoint] = useState("");
   const { projectId } = useIndex();
   const [isSlackEnpointLoading, setIsSlackEndpointLoading] = useState(true);
+  const [statuses, setStatuses] = useState([]);
   const [selectedStatuses, setSelectedStatuses] = useState([]);
 
   useEffect(() => {
     const fetchSettings = async () => {
       if (projectId) {
+        const statuses = await invoke("get-status-names", { projectId });
+        setStatuses(statuses);
         setIsSlackEndpointLoading(true);
         const data = await invoke("get-settings", { projectId });
         setSlackEndpoint(data.slackEndpoint || "");
+        setSelectedStatuses(data.selectedStatuses || []);
+        const areStatusesNeverSelected =
+          typeof data === "object" &&
+          !Object.keys(data).includes("selectedStatuses");
+
+        if (areStatusesNeverSelected && statuses.length > 0) {
+          statuses.forEach((possiblySelectedBoardStatus) => {
+            const isBoardStatusInDefaultStatuses = DEFAULT_STATUSSES.some(
+              (defaultStatus) =>
+                defaultStatus.value === possiblySelectedBoardStatus.value
+            );
+            if (isBoardStatusInDefaultStatuses) {
+              setSelectedStatuses((prevSelectedStatuses) => [
+                ...prevSelectedStatuses,
+                possiblySelectedBoardStatus,
+              ]);
+            }
+          });
+        }
         setIsSlackEndpointLoading(false);
       }
     };
---
+++ src/frontend/utils/index.js
@@ -0,0 +1,10 @@
+export const DEFAULT_STATUSSES = [
+  {
+    label: "To Do",
+    value: "To Do",
+  },
+  {
+    label: "In Progress",
+    value: "In Progress",
+  },
+];
